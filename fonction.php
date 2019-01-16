a093a6e6265eb0493079e5313a8f444cb4f89334
LalyLeCar <33251391+LalyLeCar@users.noreply.github.com>
2019-01-16 09:16:05 +0100

    maj apes

---
+++ fonction.php
@@ -0,0 +1,13 @@
+<?php
+  function displayDat() {
+    $jour = date('d');
+    $mois = date('M');
+    $annee = date('Y');
+    echo "<p>Nous sommes le:'.$jour.'/'.$mois'/'$annee'";
+    return($jour);
+  }
+
+//appel de la fonction
+displayDate();
+echo "jour:"$jour
+ ?>
